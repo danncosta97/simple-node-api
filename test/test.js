e806ed6568d94904031e65d0462f69fa61621834
danncosta97 <danielbarbosa.0797@gmail.com>
2021-11-04 00:10:24 -0300

    change to test.js - String

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ const request = require("supertest");
 const app = require("../index");
 
 describe("GET /", () => {
-    it("responds with Hello World!", (done) => {
-        request(app).get("/").expect("Hello World!", done);
+    it("responds with Web Dev Com Boas Práticas - Exercício 4 - Deploy to Heroku Automatically v2", (done) => {
+        request(app).get("/").expect("Web Dev Com Boas Práticas - Exercício 4 - Deploy to Heroku Automatically v2", done);
     })
 });
