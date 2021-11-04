97876ef42b348a324c7cd34544a7ce70abe6022e
danncosta97 <danielbarbosa.0797@gmail.com>
2021-11-04 00:06:07 -0300

    change to index.js - String

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 8000;
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Web Dev Com Boas Práticas - Exercício 4 - Deploy to Heroku Automatically');
 });
 
 app.listen(port, () => { console.log('Our App Is Up And Running!'); });
